e9b835e5b535c88b9cf80a4ce319f111ab6d6425
Steven Nunez <steven.nunez@gmail.com>
2015-03-09 16:21:01 -0400

    Update db_connection to cache connection

--- lib/support/db_connection.rb
+++ lib/support/db_connection.rb
@@ -1,13 +1,18 @@
 require 'singleton'
 class DBConnection
-  attr_accessor :dbname
+  attr_reader :dbname
   include Singleton
 
   def exec(sql, args=[])
     connection.exec_params(sql, args)
   end
 
+  def dbname=(new_dbname)
+    @dbname = new_dbname
+    @connection = PG.connect(dbname: dbname, host: 'localhost')
+  end
+
   def connection
-    PG.connect(dbname: dbname, host: 'localhost')
+    @connection ||= PG.connect(dbname: dbname, host: 'localhost')
   end
 end
